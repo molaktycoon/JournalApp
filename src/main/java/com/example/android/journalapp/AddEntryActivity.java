7b94de80b4f2343ac3987ba0ab1cd6440bc3f33b
molaktycoon <32408936+molaktycoon@users.noreply.github.com>
2018-07-01 21:13:02 +0100

    Add files via upload

---
+++ src/main/java/com/example/android/journalapp/AddEntryActivity.java
@@ -0,0 +1,11 @@
+package com.example.android.journalapp;
+
+/**
+ * Created by CODERXX on 6/30/2018.
+ */
+
+@SuppressWarnings("DefaultFileTemplate")
+class AddEntryActivity {
+}
+
+
---
+++ src/main/java/com/example/android/journalapp/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.android.journalapp;
+
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent addNewEntry = new Intent(MainActivity.this, AddEntryActivity.class);
+                startActivity(addNewEntry);
+
+            }
+        });
+    }
+
+}
